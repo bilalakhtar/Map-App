5b042cf249ff6a4b9a3e955525ea8fb92cd0e188
Bilal Akhtar <me@itsbilal.com>
2014-09-21 05:33:10 -0400

    Add initial GCM support

---
+++ app/src/main/java/com/hack/letsmeet/GcmBroadcastReceiver.java
@@ -0,0 +1,15 @@
+package com.hack.letsmeet;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+/**
+ * Created by bilal on 2014-09-21.
+ */
+public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
